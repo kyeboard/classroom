81756bf62f9bd72bf77ff081e02a7f172f7c4f18
kyeboard <me@kyeboard.me>
2023-05-20 11:37:15 +0530

    Create App icon

--- app/src/main/java/me/kyeboard/oxide/MainActivity.kt
+++ app/src/main/java/me/kyeboard/oxide/MainActivity.kt
@@ -10,9 +10,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import io.appwrite.Client
 import me.kyeboard.oxide.ui.theme.OxideTheme
 
 class MainActivity : ComponentActivity() {
+    // Initialize Appwrite SDK here
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
