76b49751cd28d49170ce817a3296ef082e028e40
kyeboard <me@kyeboard.me>
2023-05-20 10:23:18 +0530

    Create onboarding page

--- app/src/main/java/me/kyeboard/oxide/MainActivity.kt
+++ app/src/main/java/me/kyeboard/oxide/MainActivity.kt
@@ -15,32 +15,6 @@ import me.kyeboard.oxide.ui.theme.OxideTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            OxideTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    OxideTheme {
-        Greeting("Android")
+        setContentView(R.layout.activity_main)
     }
 }
