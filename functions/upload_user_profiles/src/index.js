592081a49c7c60fa3eac7a145241b6b893efbb03
kyeboard <me@kyeboard.me>
2023-05-25 12:41:00 +0530

    Update cloud function for upload_user_profile

--- functions/upload_user_profiles/src/index.js
+++ functions/upload_user_profiles/src/index.js
@@ -1,7 +1,9 @@
 const sdk = require("node-appwrite");
+const fetch = require("node-fetch")
 
 module.exports = async function (req, res) {
     const client = new sdk.Client();
+    const bucket = new sdk.Storage(client);
 
     if (
         !req.variables['APPWRITE_FUNCTION_ENDPOINT'] ||
@@ -10,7 +12,8 @@ module.exports = async function (req, res) {
         console.warn("Environment variables are not set. Function cannot use Appwrite SDK.");
     }
 
-    console.log(req.payload)
+    const users = new sdk.Users(client);
+    const payload = JSON.parse(req.variables["APPWRITE_FUNCTION_EVENT_DATA"]);
 
     client
         .setEndpoint(req.variables['APPWRITE_FUNCTION_ENDPOINT'])
@@ -18,5 +21,12 @@ module.exports = async function (req, res) {
         .setKey(req.variables['APPWRITE_FUNCTION_API_KEY'])
         .setSelfSigned(true);
 
-    res.json(req.payload);
+    try {
+        const user = await users.get(payload["userId"]);
+        const google_user = await (await fetch(`https://www.googleapis.com/oauth2/v3/userinfo?access_token=${payload["providerAccessToken"]}`)).json()
+        const image_data = await (await fetch(google_user["picture"])).text();
+        await bucket.createFile("646ef17593d213adfcf2", user["name"], sdk.InputFile.fromPlainText(image_data, "profile.jpg"))
+    } catch(e) {
+        res.json({ error: e })
+    }
 };
