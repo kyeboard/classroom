4fefb81fb9fcf966daa4b64305955d29062bcdb7
kyeboard <me@kyeboard.me>
2023-06-02 16:54:39 +0530

    Allow creating comments by the teacher and the student (only)

--- app/src/main/java/me/kyeboard/classroom/screens/SubmissionView.kt
+++ app/src/main/java/me/kyeboard/classroom/screens/SubmissionView.kt
@@ -4,6 +4,8 @@ import Comment
 import CommentAdapter
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -30,6 +32,7 @@ import java.math.BigInteger
 import java.security.MessageDigest
 
 data class SubmissionItem(val grade: Number, val comments: ArrayList<String>, val submissions: ArrayList<String>)
+data class UpdateItem(val comments: ArrayList<String>)
 
 class SubmissionView : ComponentActivity() {
     private fun getID(assignment_id: String, submission_id: String): String {
@@ -50,6 +53,23 @@ class SubmissionView : ComponentActivity() {
 
         val assignmment_id = "6475d05340bf25c2069b"
         val submission_id = "6475d05340bf25c2069b"
+
+        findViewById<Button>(R.id.submission_view_msg_send).setOnClickListener {
+            val msg = findViewById<EditText>(R.id.submission_view_msg_textbox).text.toString()
+
+            GlobalScope.launch {
+                val submission_details = databases.getDocument("classes", "64782b0c5957666e7bee", getID(assignmment_id, submission_id))
+                val current_comments = submission_details.data.tryJsonCast<SubmissionItem>()!!.comments
+
+                current_comments.add("{\"author\": \"kyeboard\", \"message\": \"$msg\"}0")
+
+                databases.updateDocument(
+                    "classes",
+                    "64782b0c5957666e7bee", getID(assignmment_id, submission_id),
+                    UpdateItem(current_comments)
+                )
+            }
+        }
 
         GlobalScope.launch {
             val submission_details = databases.getDocument("classes", "64782b0c5957666e7bee", getID(assignmment_id, submission_id))
