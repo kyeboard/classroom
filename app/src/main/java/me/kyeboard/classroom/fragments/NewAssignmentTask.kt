8f1510111ce0634e82ca3931fdead0dcf534ee5e
kyeboard <me@kyeboard.me>
2023-06-08 16:25:50 +0530

    Show assignment to be completed if the student has already turned in

--- app/src/main/java/me/kyeboard/classroom/fragments/NewAssignmentTask.kt
+++ app/src/main/java/me/kyeboard/classroom/fragments/NewAssignmentTask.kt
@@ -1,5 +1,6 @@
 package me.kyeboard.classroom.fragments
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.ContentResolver
 import android.content.Intent
@@ -7,11 +8,13 @@ import android.database.Cursor
 import android.net.Uri
 import android.os.Bundle
 import android.provider.OpenableColumns
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ListView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
@@ -19,6 +22,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import io.appwrite.extensions.tryJsonCast
 import io.appwrite.models.InputFile
+import io.appwrite.services.Account
 import io.appwrite.services.Databases
 import io.appwrite.services.Storage
 import kotlinx.coroutines.CoroutineScope
@@ -36,6 +40,8 @@ import java.io.InputStream
 import java.io.OutputStream
 import java.math.BigInteger
 import java.security.MessageDigest
+
+data class SubmissionItem(val grade: Int, val submissions: ArrayList<String>)
 
 class NewAssignmentTask : Fragment() {
     private lateinit var attachment_uri: Uri
@@ -79,11 +85,48 @@ class NewAssignmentTask : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_new_assignment_task, container, false)
 
-        val assignment_id = requireArguments().getString("assignment_id")!!
+        //val assignment_id = requireArguments().getString("assignment_id")!!
+        val assignment_id = "646f5e9c8ba221bf2df1"
+
 
         val client = get_appwrite_client(view.context)
         val databases = Databases(client)
         val storage = Storage(client)
+        val account = Account(client)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val session = account.get()
+            val id = "$assignment_id-${session.id}"
+            val hashed_id = BigInteger(1, MessageDigest.getInstance("MD5").digest(id.toByteArray())).toString(16).padStart(32, '0')
+
+            try {
+                val submission = databases.getDocument("classes", "64782b0c5957666e7bee", hashed_id).data.tryJsonCast<SubmissionItem>()!!
+
+                val attachments = arrayListOf<Attachment>()
+
+                for(attachment_id in submission.submissions) {
+                    val file = storage.getFile("647713fa9be2a68d4458", attachment_id).name
+                    attachments.add(Attachment(file.substringAfterLast('.', ""), file))
+                }
+
+                requireActivity().runOnUiThread {
+                    Toast.makeText(this@NewAssignmentTask.context, "You have already submitted bruh", Toast.LENGTH_SHORT).show()
+
+                    view.findViewById<Button>(R.id.assignment_view_add_files).visibility = View.GONE
+                    view.findViewById<Button>(R.id.assignment_view_submit_assignment).apply {
+                        text = "Already submitted"
+                        isEnabled = false
+                    }
+
+                    view.findViewById<RecyclerView>(R.id.assignment_view_submissions_list).apply {
+                        adapter = AttachmentAdapter(attachments)
+                        layoutManager = LinearLayoutManager(this@NewAssignmentTask.context)
+                    }
+                }
+            } catch(e: Exception) {
+                Log.e("ee", e.message.toString())
+            }
+        }
 
         val description = view.findViewById<TextView>(R.id.assignment_view_description)
         val listview = view.findViewById<RecyclerView>(R.id.assignment_view_attachment_list)
@@ -114,15 +157,19 @@ class NewAssignmentTask : Fragment() {
             }
         }
 
+
         view.findViewById<Button>(R.id.assignment_view_add_files).setOnClickListener {
             pickFiles.launch(intent)
         }
 
         view.findViewById<Button>(R.id.assignment_view_submit_assignment).setOnClickListener {
             CoroutineScope(Dispatchers.IO).launch {
-                val id = "$assignment_id-64735c4d22a9a24664e8"
+                val current_user = account.get()
+                val id = "$assignment_id-${current_user.id}"
                 val hashed_id = BigInteger(1, MessageDigest.getInstance("MD5").digest(id.toByteArray())).toString(16).padStart(32, '0')
-                uploadToAppwriteStorage(hashed_id, view.context.contentResolver, attachment_uri, storage)
+                val attachments = uploadToAppwriteStorage("unique()", view.context.contentResolver, attachment_uri, storage)
+
+                databases.createDocument("classes", "64782b0c5957666e7bee", hashed_id, SubmissionItem(0, arrayListOf(attachments)))
             }
         }
 
