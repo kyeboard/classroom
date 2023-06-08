07aa39f9d6476de3f735ffaa0662e5c9abfd0501
kyeboard <me@kyeboard.me>
2023-06-08 17:42:53 +0530

    Remove all unnecessary files

--- app/src/main/java/me/kyeboard/classroom/adapters/submission_comment.kt
+++ app/src/main/java/me/kyeboard/classroom/adapters/submission_comment.kt
@@ -1,36 +1,14 @@
-import android.Manifest
-import android.content.Intent
-import android.net.Uri
-import android.os.Environment
-import android.os.StrictMode
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.content.FileProvider
 import androidx.recyclerview.widget.RecyclerView
-import io.appwrite.models.Membership
-import io.appwrite.services.Storage
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import me.kyeboard.classroom.R
-import me.kyeboard.classroom.utils.get_appwrite_client
-import java.io.File
-import java.io.FileOutputStream
 
 data class Comment(val author: String, val message: String)
 
 class CommentAdapter(private val dataSet: ArrayList<Comment>) :
     RecyclerView.Adapter<CommentAdapter.ViewHolder>() {
-
-    /**
-     * Provide a reference to the type of views that you are using
-     * (custom ViewHolder)
-     */
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val username: TextView
         val message: TextView
