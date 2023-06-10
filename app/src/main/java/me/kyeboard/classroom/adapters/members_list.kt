3c627e77b9c8dacfb677e4f5eb85aea9a258dd13
kyeboard <me@kyeboard.me>
2023-06-10 12:23:10 +0530

    Show user pfp and join date in members list

--- app/src/main/java/me/kyeboard/classroom/adapters/members_list.kt
+++ app/src/main/java/me/kyeboard/classroom/adapters/members_list.kt
@@ -6,9 +6,11 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 import io.appwrite.models.Membership
 import io.appwrite.services.Storage
 import kotlinx.coroutines.GlobalScope
@@ -17,6 +19,8 @@ import me.kyeboard.classroom.R
 import me.kyeboard.classroom.utils.get_appwrite_client
 import java.io.File
 import java.io.FileOutputStream
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class MembersList(private val dataSet: List<Membership>) :
     RecyclerView.Adapter<MembersList.ViewHolder>() {
@@ -27,9 +31,13 @@ class MembersList(private val dataSet: List<Membership>) :
      */
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val username: TextView
+        val pfp: ImageView
+        val member_since: TextView
 
         init {
             username = view.findViewById(R.id.members_list_item_username)
+            pfp = view.findViewById(R.id.member_list_item_pfp)
+            member_since = view.findViewById(R.id.members_list_item_member_since)
         }
     }
 
@@ -46,7 +54,14 @@ class MembersList(private val dataSet: List<Membership>) :
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
         val data = dataSet[position]
 
+        val inputFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSX", Locale.US)
+        val outputFormat = SimpleDateFormat("d MMMM, yyyy", Locale.US)
+
+        val date = inputFormat.parse(data.joined)!!
+
         viewHolder.username.text = data.userName
+        viewHolder.member_since.text = "Member since ${outputFormat.format(date)}"
+        Picasso.get().load("https://cloud.appwrite.io/v1/storage/buckets/646ef17593d213adfcf2/files/${data.userId}/view?project=fryday").into(viewHolder.pfp)
     }
 
     // Return the size of your dataset (invoked by the layout manager)
