b90cc6da9f352da760fcd76c54d106731a32075a
kyeboard <me@kyeboard.me>
2023-06-11 16:53:10 +0530

    Add a tablayout rather than headers to differentiate missed/assigned tasks

--- app/src/main/java/me/kyeboard/classroom/adapters/class_assignments.kt
+++ app/src/main/java/me/kyeboard/classroom/adapters/class_assignments.kt
@@ -19,32 +19,22 @@ import java.util.Locale
 import java.util.concurrent.TimeUnit
 import kotlin.math.abs
 
-data class Assignment(val author: String, val title: String, val due_date: String, val `$id`: String)
+data class Assignment(val author: String, val title: String, val due_date: Date, val `$id`: String)
 
-val isoDateFormatter = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
 val dateFormat = SimpleDateFormat("d MMMM, yyyy", Locale.ENGLISH)
 
-class AssignmentAdapter(private val dataSet: List<Document>, private val onClick: (id: String) -> Unit) :
+class AssignmentAdapter(private val dataSet: List<Assignment>, private val onClick: (id: String) -> Unit) :
     RecyclerView.Adapter<AssignmentAdapter.ViewHolder>() {
 
-    val today = Date()
-    var previous_title = ""
-
-    /**
-     * Provide a reference to the type of views that you are using
-     * (custom ViewHolder)
-     */
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val title: TextView
         val time: TextView
         val parent: ConstraintLayout
-        val timeDiff: TextView
 
         init {
             title = view.findViewById(R.id.class_assignment_item_title)
             parent = view.findViewById(R.id.assignment_item_parent)
             time = view.findViewById(R.id.class_assignment_item_time)
-            timeDiff = view.findViewById(R.id.class_assignments_time_header)
         }
     }
 
@@ -57,44 +47,16 @@ class AssignmentAdapter(private val dataSet: List<Document>, private val onClick
         return ViewHolder(view)
     }
 
-    fun getDaysDifference(newDate: String): Long {
-        val parsedDate = isoDateFormatter.parse(newDate)
-        val diff = parsedDate.time - today.time
-
-        return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS)
-    }
-
     // Replace the contents of a view (invoked by the layout manager)
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-        val data = dataSet[position].data.tryJsonCast<Assignment>()!!
-
-        Log.d("tt", "Reloading data")
-
-        val days_difference = getDaysDifference(data.due_date)
-        val new_title = getTitleForDiff(days_difference)
+        val data = dataSet[position]
 
         viewHolder.parent.setOnClickListener {
             onClick(data.`$id`)
         }
 
         viewHolder.title.text = data.title
-        viewHolder.time.text = dateFormat.format(isoDateFormatter.parse(dataSet[position].createdAt)!!)
-
-        if(previous_title != new_title) {
-            previous_title = new_title
-            viewHolder.timeDiff.text = new_title
-        } else {
-            viewHolder.timeDiff.visibility = View.GONE
-        }
-    }
-
-    private fun getTitleForDiff(difference: Long): String {
-        return when {
-            difference < 0 -> "Missed"
-            difference == 0L -> "Today"
-            difference == 1L -> "Tomorrow"
-            else -> "Others"
-        }
+        viewHolder.time.text = "Due on ${dateFormat.format(data.due_date)}"
     }
 
     // Return the size of your dataset (invoked by the layout manager)
--- app/src/main/java/me/kyeboard/classroom/fragments/ClassDashboardAssignments.kt
+++ app/src/main/java/me/kyeboard/classroom/fragments/ClassDashboardAssignments.kt
@@ -1,16 +1,21 @@
 package me.kyeboard.classroom.fragments
 
+import Assignment
 import AssignmentAdapter
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
+import io.appwrite.Client
 import io.appwrite.Query
+import io.appwrite.extensions.tryJsonCast
 import io.appwrite.services.Databases
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -18,8 +23,13 @@ import kotlinx.coroutines.launch
 import me.kyeboard.classroom.R
 import me.kyeboard.classroom.screens.AssignmentView
 import me.kyeboard.classroom.utils.get_appwrite_client
+import java.time.LocalDate
+import java.util.Date
 
 class ClassDashboardAssignments : Fragment() {
+    private lateinit var client: Client
+    private lateinit var databases: Databases
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -28,24 +38,66 @@ class ClassDashboardAssignments : Fragment() {
         val view = inflater.inflate(R.layout.fragment_class_dashboard_assignments, container, false)
 
         val recyclerView = view.findViewById<RecyclerView>(R.id.classdashboard_assignments_recyclerview)
-        val client = get_appwrite_client(view.context)
-        val databases = Databases(client)
+        client = get_appwrite_client(view.context)
+        databases = Databases(client)
+
+        val today = Date()
+
+        // Filters
+        val missed_filter: (Date, Boolean) -> Boolean = { date, _ -> date.before(today) }
+        val assigned_filter: (Date, Boolean) -> Boolean = { date, _ -> date == today }
+        val submitted_filter: (Date, Boolean) -> Boolean = { _, has_submitted -> has_submitted }
 
         CoroutineScope(Dispatchers.IO).launch {
-            try {
-                val data = databases.listDocuments("classes", "646f432ad59caafabf74", listOf(Query.orderAsc("due_date"))).documents
-                val adapter = AssignmentAdapter(data, this@ClassDashboardAssignments::openAssignment)
-
-                activity?.runOnUiThread {
-                    recyclerView.adapter = adapter
-                    recyclerView.layoutManager = LinearLayoutManager(view.context)
-                }
-            } catch(e: Exception) {
-                Log.e("eee", e.message.toString())
-            }
+            populateAssignments(recyclerView, view, assigned_filter)
         }
 
+        // Handle tab layout chances
+        view.findViewById<TabLayout>(R.id.assignments_tablayout).addOnTabSelectedListener(object : OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                val filter = when(tab!!.position) {
+                    0 -> missed_filter
+                    1 -> assigned_filter
+                    else -> submitted_filter
+                }
+
+                Log.d("tt", tab!!.position.toString())
+
+                CoroutineScope(Dispatchers.IO).launch {
+                    populateAssignments(recyclerView, view, filter)
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
+        })
+
         return view
+    }
+
+    private suspend fun populateAssignments(recyclerView: RecyclerView, view: View, filter: (Date, Boolean) -> Boolean) {
+        try {
+            val data = databases.listDocuments("classes", "646f432ad59caafabf74", listOf(Query.orderAsc("due_date"))).documents
+            val parsed_data = arrayListOf<Assignment>()
+
+            for(item in data) {
+                val parsed = item.data.tryJsonCast<Assignment>()!!
+
+                if(filter(parsed.due_date, true)) {
+                    parsed_data.add(parsed)
+                }
+            }
+
+            val adapter = AssignmentAdapter(parsed_data, this@ClassDashboardAssignments::openAssignment)
+
+            activity?.runOnUiThread {
+                recyclerView.adapter = adapter
+                recyclerView.layoutManager = LinearLayoutManager(view.context)
+            }
+        } catch(e: Exception) {
+            Log.e("eee", e.message.toString())
+        }
     }
 
     private fun openAssignment(id: String) {
