83e794665c5c2f82c4e5e3a4988a8eb4e5b21399
kyeboard <me@kyeboard.me>
2023-06-12 17:31:21 +0530

    legacy: Reduce minSdk version to API lvl 19 (Android 4.4)

--- app/src/main/java/me/kyeboard/classroom/screens/ClassPeople.kt
+++ app/src/main/java/me/kyeboard/classroom/screens/ClassPeople.kt
@@ -3,7 +3,9 @@ package me.kyeboard.classroom.screens
 import MembersList
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.ProgressBar
@@ -24,6 +26,7 @@ import androidx.core.content.res.ResourcesCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import io.appwrite.Client
 import io.appwrite.services.Teams
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -33,44 +36,59 @@ import me.kyeboard.classroom.R
 import me.kyeboard.classroom.screens.ui.theme.ClassroomTheme
 import me.kyeboard.classroom.utils.get_appwrite_client
 
+val email_check_regex = Regex("^\\w+([.-]?\\w+)*@\\w+([.-]?\\w+)*(\\.\\w{2,})+$")
+
 class ClassPeople : ComponentActivity() {
+    private lateinit var client: Client
+    private lateinit var teams: Teams
+    private lateinit var classId: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Set view
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_classppl)
 
         actionBar?.hide()
 
-        val classId = intent.extras!!.getString("class_id")!!
-        val accentColor = intent.extras!!.getString("accent_color")!!
-
-        val loading = findViewById<ProgressBar>(R.id.members_list_loading)
+        // Get items from extras
+        val extras = intent.extras!!
+        classId = extras.getString("class_id")!!
+        val accentColor = extras.getString("accent_color")!!
 
         window.statusBarColor = Color.parseColor(accentColor)
-        findViewById<ConstraintLayout>(R.id.classppl_topbar).background.setTint(Color.parseColor(accentColor))
 
+        // View holders
+        val loading = findViewById<ProgressBar>(R.id.members_list_loading)
         val view = findViewById<RecyclerView>(R.id.members_list)
-        val client = get_appwrite_client(this)
-        val teams = Teams(client)
-
         val refreshLayout = findViewById<SwipeRefreshLayout>(R.id.members_list_refresh_layout)
 
+        // Set accent
+        findViewById<ConstraintLayout>(R.id.classppl_topbar).background.setTint(Color.parseColor(accentColor))
+
+        // Initiate appwrite services
+        client = get_appwrite_client(applicationContext)
+        teams = Teams(client)
+
+        // Handle refreshing
         refreshLayout.setOnRefreshListener {
             CoroutineScope(Dispatchers.IO).launch {
-                populateList(teams, classId, view, loading, false)
+                populateList(view, loading, false)
                 refreshLayout.isRefreshing = false
             }
         }
 
+        // Handle destroy self
         findViewById<ImageView>(R.id.newclass_destory_self).setOnClickListener {
             finish()
         }
 
-        CoroutineScope(Dispatchers.IO).launch { populateList(teams, classId, view, loading) }
+        // Initial load
+        CoroutineScope(Dispatchers.IO).launch { populateList(view, loading) }
 
-        val email_check_regex = Regex("^\\w+([.-]?\\w+)*@\\w+([.-]?\\w+)*(\\.\\w{2,})+$")
-
+        // Handle sending invite
         findViewById<ImageView>(R.id.send_invite).setOnClickListener {
             val email_address = findViewById<EditText>(R.id.classppl_invite_email).text.toString()
+            val invite_as_teacher = findViewById<CheckBox>(R.id.invite_as_teacher).isChecked
 
             if(email_address.isBlank()) {
                 Toast.makeText(this, "Email address cannot be empty!", Toast.LENGTH_SHORT).show()
@@ -85,11 +103,20 @@ class ClassPeople : ComponentActivity() {
             }
 
             // Send an invite
-            GlobalScope.launch {
-                teams.createMembership(classId, email_address, listOf(), "https://fryday.vercel.app")
+            CoroutineScope(Dispatchers.IO).launch {
+                val roles = if(invite_as_teacher) {
+                    listOf("teacher")
+                } else {
+                    listOf()
+                }
 
-                populateList(teams, classId, view, loading)
+                // Send an invite
+                teams.createMembership(classId, email_address, roles, "https://fryday.vercel.app")
 
+                // Refresh layout
+                populateList(view, loading)
+
+                // Send a toast msg
                 runOnUiThread {
                     Toast.makeText(this@ClassPeople, "Successfully sent an invite!", Toast.LENGTH_SHORT).show()
                 }
@@ -97,7 +124,7 @@ class ClassPeople : ComponentActivity() {
         }
     }
 
-    private suspend fun populateList(teams: Teams, classId: String, view: RecyclerView, loading: ProgressBar, showLoading: Boolean = true) {
+    private suspend fun populateList(view: RecyclerView, loading: ProgressBar, showLoading: Boolean = true) {
         runOnUiThread {
             if(showLoading) {
                 loading.visibility = View.VISIBLE
